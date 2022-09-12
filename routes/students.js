f2f2a31467c3190e474577c9b5c3c2ce468e1311
Parker Macdonald <larrythepig123@gmail.com>
2022-09-12 11:10:44 -0600

    remove sequelize

--- routes/students.js
+++ routes/students.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("sqlite::./data/fbla:")
-
-
 router.get('/', function(req, res) {
     res.send('students')
 })
 
 router.route('/:id')
     .get(function(req, res) {
-        
+        res.send(req.params.id)
     })
     .put(function(req, res) {
         res.send(req.params.id.toString())
